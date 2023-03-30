b9e104d2a4f37a5872a9b2133571c1f1de8a2842
Anastasia Torgova <anastasia.torgova@mail.ru>
2023-03-30 21:03:26 +0400

    rename classes

--- src/components/OrderSummary/OrderSummary.tsx
+++ src/components/OrderSummary/OrderSummary.tsx
@@ -2,14 +2,14 @@ import styles from "./OrderSummary.module.css";
 
 export function OrderSummary() {
   return (
-    <div className={styles.myCard}>
+    <div className={styles['my-card']}>
       <img
         className={styles.hero}
         src="./assets/illustration-hero.svg"
         alt="our hero"
       />
 
-      <div className={styles.cardContent}>
+      <div className={styles['card-content']}>
         <div className={styles.description}>
           <h1>Order Summary</h1>
           <p>
@@ -18,27 +18,27 @@ export function OrderSummary() {
           </p>
         </div>
 
-        <div className={styles.miniCard}>
-          <div className={styles.miniCardDetail}>
+        <div className={styles['mini-card']}>
+          <div className={styles['mini-card-detail']}>
             <img src="./assets/icon-music.svg" alt="icon-music" />
 
             <div>
-              <p className={styles.itemName}>Annual Plan</p>
-              <p className={styles.itemPrice}>$59.99/year</p>
+              <p className={styles['item-name']}>Annual Plan</p>
+              <p className={styles['item-price']}>$59.99/year</p>
             </div>
           </div>
 
-          <a className={styles.changeItem} href="###">
+          <a className={styles['change-item']} href="###">
             Change
           </a>
         </div>
 
         <div className={styles.submission}>
-          <a className={styles.proceedBtn} href="#">
+          <a className={styles['proceed-btn']} href="#">
             Proceed to Payment
           </a>
 
-          <a href="##" className={styles.cancelOrder}>
+          <a href="##" className={styles['cancel-order']}>
             Cancel Order
           </a>
         </div>
