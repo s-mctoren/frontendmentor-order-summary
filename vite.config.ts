80962ca372f2b68eb946c928fb1850319a686b1f
Anastasia Torgova <anastasia.torgova@mail.ru>
2023-03-31 20:52:31 +0400

    change base for vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/frontendmentor-order-summary/",
   plugins: [react()],
-  base: "https://s-mctoren.github.io/frontendmentor-order-summary/",
 });
